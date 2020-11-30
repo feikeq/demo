a9f606e6b0dde675d2df05bbd62abc4b021bbaea
lq <feikeq@163.com>
2020-11-30 18:18:52 +0800

    middleware 中间件

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="html5">
+    <!-- {{ asyncDataName + "|" + dataName }} -->
     <Header />
     <section class="main">
       <Nuxt />
@@ -9,51 +10,16 @@
 </template>
 
 <script>
-import { mapMutations } from "vuex";
-import { UAParser } from "ua-parser-js";
-
+//需要注意的是在默认布局里不要加入头部信息，只是关于<template>标签下的内容统一订制。
 export default {
   asyncData({ req, res, params }) {
     // layouts 好像没有asyncData这个方法
-    return { name: "路由地址fullPath", ua_name: "aaaaa" };
-  },
-  methods: {
-    ...mapMutations({
-      add: "todos/add",
-    }),
+    return { asyncDataName: "这是asyncData()的Name" };
   },
   data() {
-    //如果组件的数据不需要异步获取或处理，可以直接返回指定的字面对象作为组件的数据。
-    console.log("------layouts------");
-
-    var LUA = new UAParser().getBrowser();
-    console.log("default.vue [data] ua_parser", LUA);
-
-    // console.log("data访问不了asyncData的值呀", this.name);
-    return { foo: "bar", LUA };
-  },
-  fetch() {
-    // fetch方法用于在呈现页面之前填充存储
-  },
-  head() {
-    // 为此页设置元标记
     return {
-      title: "网站的标题(" + this.$route.params.id + ")",
-      meta: [
-        { name: "keywords", content: "HTML,CSS,XML,JavaScript" },
-        { name: "author", content: "FK68.net" },
-        {
-          hid: "description", //为了避免子组件中的 meta 标签不能正确覆盖父组件中相同的标签而产生重复的现象，建议利用 hid 键为 meta 标签配一个唯一的标识编号。
-          name: "description",
-          content: "网站的描述...",
-        },
-      ],
+      dataName: "这是Data()的Name",
     };
-  },
-  computed: {
-    todos() {
-      return this.$store.state.todos.list;
-    },
   },
 };
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,15 @@
       <Logo />
 
       <h1 class="title">
-        {{ fkname + "-" + "UA:" + UA.name }}
+        {{ FKasyncData + "-" + "asyncData()" + UA.name }}
+        <pre>
+            {{ UA }}
+        </pre>
+        <br />
+        {{ FKdata + "-" + "data()" + LUA.name }}
+        <pre>
+            {{ LUA }}
+        </pre>
       </h1>
       <div class="links">
         <NLink to="./users/">users</NLink>
@@ -14,9 +22,24 @@
 </template>
 
 <script>
+import { mapMutations } from "vuex";
 import { UAParser } from "ua-parser-js";
+
 export default {
+  /*
+    中间件：允许您定义一个自定义函数运行在一个页面或一组页面渲染之前。
+    每一个中间件应放置在 middleware/ 目录。
+    文件名的名称将成为中间件名称 (middleware/auth.js将成为 auth 中间件)。
+    中间件执行流程顺序：
+    1.nuxt.config.js
+    2.匹配布局
+    3.匹配页面
+    中间件可以异步执行,只需要返回一个 Promise 或使用第 2 个 callback 作为第一个参数
+    */
+  middleware: "device", //中间件 可以在 pages 或者 layouts 或者 nuxt.config.js的路由router中 使用
   asyncData({ app, query, params, req, res, error }) {
+    // layouts 好像没有asyncData这个方法
+
     // 每次加载组件前调用
     // 由于asyncData方法是在组件 初始化 前被调用的，所以在方法内是没有办法通过 this 来引用组件的实例对象会提示undefined。
     console.log(".......asyncData......");
@@ -27,7 +50,52 @@ export default {
     var UA = new UAParser().getBrowser();
     console.log("index.vue [asyncData] ua_parser", UA);
 
-    return { fkname: "FK", UA };
+    return { FKasyncData: "肥客FK项目", UA };
+  },
+  methods: {
+    ...mapMutations({
+      add: "todos/add",
+    }),
+  },
+  data() {
+    //如果组件的数据不需要异步获取或处理，可以直接返回指定的字面对象作为组件的数据。
+    console.log("------pages------");
+
+    var LUA = new UAParser().getBrowser();
+    console.log("default.vue [data] ua_parser", LUA);
+    console.log("------------");
+    console.log(LUA.name);
+    console.log("------------");
+
+    console.log("data访问不了asyncData的值呀", this.FKasyncData);
+    return { FKdata: "测试数据bar", LUA };
+  },
+  fetch() {
+    // fetch方法用于在呈现页面之前填充存储
+  },
+  head() {
+    // 为此页设置元标记
+    return {
+      title: "网站的标题2(" + this.$route.params.id + ")",
+      meta: [
+        { name: "keywords", content: "HTML,CSS,XML,JavaScript" },
+        { name: "author", content: "FK68.net" },
+        {
+          hid: "description", //为了避免子组件中的 meta 标签不能正确覆盖父组件中相同的标签而产生重复的现象，建议利用 hid 键为 meta 标签配一个唯一的标识编号。
+          name: "description",
+          content: "网站的描述...",
+        },
+      ],
+    };
+  },
+  computed: {
+    todos() {
+      return this.$store.state.todos.list;
+    },
+  },
+  created() {
+    console.log("created访问asyncData的值", this.FKasyncData, this.UA.name);
+    console.log("created访问data的值", this.FKdata, this.LUA.name);
   },
 };
 </script>
