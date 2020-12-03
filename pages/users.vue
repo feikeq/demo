50efab040a45aecfcaba47d9be04e735c99e15d3
lq <feikeq@163.com>
2020-12-03 10:37:49 +0800

    nuxt-child

---
+++ pages/users.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div style="background: #8bc34a; color: #630a7f">
+      <b>userv.vue</b>
+      <p>
+        然后在一级页面中使用 nuxt-child 来显示子页面，就像使用 router-view 一样
+      </p>
+    </div>
+    <!-- 也可以用 <router-view></router-view> -->
+
+    <nuxt-child></nuxt-child>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
